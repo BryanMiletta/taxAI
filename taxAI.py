1419ab63cbd73ad4ce44d0a76957fa23b6e3239e
Bryan Miletta <bmiletta@uwm.edu>
2023-06-21 10:23:20 -0500

    updated taxAI.py to generate local csv file for training.

--- taxAI.py
+++ taxAI.py
@@ -1,69 +1,115 @@
 #Bryan Miletta - CS995 Capstone
 #TaxAI
 #level: Sandbox
-#summary:this code is testing setting up a Q/A BERT model that allows user input. It is not fine tuned yet
+#summary:this code is testing setting up a Q/A BERT model that allows user input. 
 
 ### ### ### Import Libraries
 import torch
+import pandas as pd
+import numpy as np
+### ### ###
+### ### ### Transformers libray download and selections 
+from transformers import BertForQuestionAnswering
+from transformers import BertTokenizer 
 ### ### ###
 
-### ### ### Transformers libray download and selections 
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
-from transformers import pipeline 
-### ### ###
+# data
+coqa = pd.read_json('http://downloads.cs.stanford.edu/nlp/data/coqa/coqa-train-v1.0.json')
+coqa.head()
+# data cleaning
+del coqa["version"]
+
+#required columns in our dataframe
+cols = ["text","question","answer"]
+#list of lists to create our dataframe
+comp_list = []
+for index, row in coqa.iterrows():
+    for i in range(len(row["data"]["questions"])):
+        temp_list = []
+        temp_list.append(row["data"]["story"])
+        temp_list.append(row["data"]["questions"][i]["input_text"])
+        temp_list.append(row["data"]["answers"][i]["input_text"])
+        comp_list.append(temp_list)
+new_df = pd.DataFrame(comp_list, columns=cols) 
+#saving the dataframe to csv file for further loading
+new_df.to_csv("CoQA_data.csv", index=False)
+
+# load from csv
+data = pd.read_csv("CoQA_data.csv")
+data.head()
+
+print("Number of question and answers: ", len(data))
+
+
 ### ### ### - model and tokenizer set up
-# Choose a pre-trained BERT model - I have two setup, bert base and pipeline
-model_name = "bert-base-uncased"
-nlp = pipeline("question-answering")
+# Choose a pre-trained BERT model 
+model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
 
 # Load the tokenizer and model for the bert-base model
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
 ### ### ###
+
+# asking a question
+def question_answer(question, text):
+    
+    #tokenize question and text as a pair
+    input_ids = tokenizer.encode(question, text)
+    
+    #string version of tokenized ids
+    tokens = tokenizer.convert_ids_to_tokens(input_ids)
+    
+    #segment IDs
+    #first occurence of [SEP] token
+    sep_idx = input_ids.index(tokenizer.sep_token_id)
+    #number of tokens in segment A (question)
+    num_seg_a = sep_idx+1
+    #number of tokens in segment B (text)
+    num_seg_b = len(input_ids) - num_seg_a
+    
+    #list of 0s and 1s for segment embeddings
+    segment_ids = [0]*num_seg_a + [1]*num_seg_b
+    assert len(segment_ids) == len(input_ids)
+    
+    #model output using input_ids and segment_ids
+    output = model(torch.tensor([input_ids]), token_type_ids=torch.tensor([segment_ids]))
+    
+    #reconstructing the answer
+    answer_start = torch.argmax(output.start_logits)
+    answer_end = torch.argmax(output.end_logits)
+    if answer_end >= answer_start:
+        answer = tokens[answer_start]
+        for i in range(answer_start+1, answer_end+1):
+            if tokens[i][0:2] == "##":
+                answer += tokens[i][2:]
+            else:
+                answer += " " + tokens[i]
+                
+    if answer.startswith("[CLS]"):
+        answer = "Unable to find the answer to your question."
+    
+    print("\nPredicted answer:\n{}".format(answer.capitalize()))
 
 ### ### ### UI
 # Initial input of context from the user
-context = input("Enter your context: ")
-question = "" #initilizes question
+#context = input("Enter your context: ")
+text = input("Please enter your text: \n")
+question = input("\nPlease enter your question: \n")
 
-# while loop to continue to prompt question/answers until user enters "exit"
-while question != "exit":
-    # promt the user for a question
-    question = input("Enter your question (type exit to terminate the session): ")
-   
-    # watch for request to exit the program
-    if question == "exit": 
-        print("OK we are going to exit this session.") 
-        break   # terminates the program
+while True:
+    question_answer(question, text)
     
-    # Tokenize the input for bert-base
-    inputs = tokenizer.encode_plus(question, context, return_tensors="pt")
-    print("***debug output: ",inputs)
-    print()
-
-    ### runs pipeline model
-    answer_pipeline = nlp(question=question, context=context)
-    ###
+    flag = True
+    flag_N = False
     
-    # Perform the question-answering task
-    outputs = model(**inputs)
-    start_scores = outputs.start_logits
-    end_scores = outputs.end_logits
-
-    # Get the predicted answer span
-    answer_start = torch.argmax(start_scores)
-    answer_end = torch.argmax(end_scores) + 1
-    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(inputs["input_ids"][0][answer_start:answer_end]))
-
-    # Output
-    print("Question:", question)
-    print("Answer for bert-base-uncased:", answer)
-    print("Answer for pipeline:", answer_pipeline["answer"])
-    print("Answer for pipeline:", answer_pipeline["score"])
-    print()
-
-# endWhile
-
-
-
-
+    while flag:
+        response = input("\nDo you want to ask another question based on this text (Y/N)? ")
+        if response[0] == "Y":
+            question = input("\nPlease enter your question: \n")
+            flag = False
+        elif response[0] == "N":
+            print("\nBye!")
+            flag = False
+            flag_N = True
+            
+    if flag_N == True:
+        break
