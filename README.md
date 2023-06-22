79e3b0ceb305512619b33f9fe3f4cf34fb88843b
Bryan Miletta <bamiletta@gmail.com>
2023-06-22 09:06:18 -0500

    updated README and taxAI_three scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # taxAI
+# Bryan Miletta
+# University of Wisconsin Milwaukee
+# Summer 2023 Capstone
+# taxAI is xxx
---
+++ taxAI_three.py
@@ -0,0 +1,6 @@
+#Bryan Miletta - CS995 Capstone
+#TaxAI
+#level: Sandbox
+#summary:this code follows example from https://www.tensorflow.org/tfmodels/nlp/fine_tune_bert 
+
+### ### ### Import Libraries
