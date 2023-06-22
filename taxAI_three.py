6287f0cfe5a39b0bffd4390d69f0e40fbeb835d7
Bryan Miletta <bamiletta@gmail.com>
2023-06-22 10:22:47 -0500

    fine-tuned the model and set up training

--- taxAI_three.py
+++ taxAI_three.py
@@ -113,3 +113,99 @@ glue_validation = glue['validation'].map(bert_inputs_processor).prefetch(1)
 glue_test = glue['test'].map(bert_inputs_processor).prefetch(1)
 
 ### ### ### Build, train and export the model
+# download the config file for pre-trained BERT model
+import json
+bert_config_file = os.path.join(gs_folder_bert, "bert_config.json")
+config_dict = json.loads(tf.io.gfile.GFile(bert_config_file).read())
+config_dict
+
+encoder_config = tfm.nlp.encoders.EncoderConfig({
+    'type':'bert',
+    'bert': config_dict
+})
+
+bert_encoder = tfm.nlp.encoders.build_encoder(encoder_config)
+bert_encoder
+
+bert_classifier = tfm.nlp.models.BertClassifier(network=bert_encoder, num_classes=2)
+
+# run it on a test batch of data 10 examples from the training set. ouput is the logits for the two classes
+bert_classifier(
+    example_inputs, training=True).numpy()[:10]
+
+tf.keras.utils.plot_model(bert_encoder, show_shapes=True, dpi=48)
+
+### Restore the encoder weights
+checkpoint = tf.train.Checkpoint(encoder=bert_encoder)
+checkpoint.read(
+    os.path.join(gs_folder_bert, 'bert_model.ckpt')).assert_consumed()
+
+### Set up the optimizer
+# Set up epochs and steps
+epochs = 5
+batch_size = 32
+eval_batch_size = 32
+
+train_data_size = info.splits['train'].num_examples
+steps_per_epoch = int(train_data_size / batch_size)
+num_train_steps = steps_per_epoch * epochs
+warmup_steps = int(0.1 * num_train_steps)
+initial_learning_rate=2e-5
+
+# linear decay from initial_learning_rate to zero over num_train_steps
+linear_decay = tf.keras.optimizers.schedules.PolynomialDecay(
+    initial_learning_rate=initial_learning_rate,
+    end_learning_rate=0,
+    decay_steps=num_train_steps)
+#warmup to that value over warmup_steps
+warmup_schedule = tfm.optimization.lr_schedule.LinearWarmup(
+    warmup_learning_rate = 0,
+    after_warmup_lr_sched = linear_decay,
+    warmup_steps = warmup_steps
+)
+# overall schedule looks like:
+x = tf.linspace(0, num_train_steps, 1001)
+y = [warmup_schedule(xi) for xi in x]
+plt.plot(x,y)
+plt.xlabel('Train step')
+plt.ylabel('Learning rate')
+
+# Use tf.keras.optimizers.experimental.AdamW to instantiate the optimizer with that schedule:
+optimizer = tf.keras.optimizers.experimental.Adam(
+    learning_rate = warmup_schedule)
+
+### Train the model
+metrics = [tf.keras.metrics.SparseCategoricalAccuracy('accuracy', dtype=tf.float32)]
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+bert_classifier.compile(
+    optimizer=optimizer,
+    loss=loss,
+    metrics=metrics)
+bert_classifier.evaluate(glue_validation)
+bert_classifier.fit(
+      glue_train,
+      validation_data=(glue_validation),
+      batch_size=32,
+      epochs=epochs)
+
+### ### Now run the fine-tuned model on a custom example to see that it works:
+# start by encoding some sentence pairs
+my_examples = {
+        'sentence1':[
+            'The rain in Spain falls mainly on the plain.',
+            'Look I fine tuned BERT.'],
+        'sentence2':[
+            'It mostly rains on the flat lands of Spain.',
+            'Is it working? This does not match.']
+    }
+# the model should report class 1 "match" for the first example and class 0 "no-match" for the second
+ex_packed = bert_inputs_processor(my_examples)
+my_logits = bert_classifier(ex_packed, training=False)
+
+result_cls_ids = tf.argmax(my_logits)
+result_cls_ids
+tf.gather(tf.constant(info.features['label'].names), result_cls_ids)
+
+### Export the model
+
