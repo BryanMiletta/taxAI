6462c3a23317d7b1fc6158857954e265a8a0293d
Bryan Miletta <bamiletta@gmail.com>
2023-06-25 07:31:54 -0500

    Created new .py file to run BERT+SQuAD model

---
+++ taxAI_four.py
@@ -0,0 +1,7 @@
+#Bryan Miletta - CS995 Capstone
+#TaxAI
+#level: Sandbox
+#summary:this code is testing setting up a Q/A BERT model that allows user input. 
+#it creates a locally saved .csv file, reformats it for training, trains, and then answers questions
+
+### ### ### Import Libraries
