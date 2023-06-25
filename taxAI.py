c020d318cc1088e8ca6876b5e96286e21d053c0c
Bryan Miletta <bamiletta@gmail.com>
2023-06-25 13:17:02 -0500

    minor update

--- taxAI.py
+++ taxAI.py
@@ -6,122 +6,81 @@
 
 
 ### ### ### Import Libraries
-import random
-import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense
-from transformers import AutoTokenizer, TFAutoModel
-### ### ###
+from transformers import BertTokenizer, TFBertForQuestionAnswering, TFBertForQuestionAnswering, TFBertModel
+from transformers import squad_convert_examples_to_features
+from transformers.data.processors.squad import SquadV2Processor
+from transformers.data.metrics.squad_metrics import squad_evaluate
 
-### ### ### Data processing
-AUTOTUNE = tf.data.AUTOTUNE
-batch_size = 32
-seed = 42
-raw_train_ds = tf.keras.utils.text_dataset_from_directory(
-    'db/train',
-    batch_size=batch_size,
-    validation_split=0.2,
-    subset='training',
-    seed=seed)
-class_names = raw_train_ds.class_names
-train_ds = raw_train_ds.cache().prefetch(buffer_size=AUTOTUNE)
-val_ds = tf.keras.utils.text_dataset_from_directory(
-    'db/train',
-    batch_size=batch_size,
-    validation_split=0.2,
-    subset='validation',
-    seed=seed)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-test_ds = tf.keras.utils.text_dataset_from_directory(
-    'db/test',
-    batch_size=batch_size)
-test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
-### ### ### END Data processing 
+# Load the BERT model and tokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-### ### ### Functions to read data
-def readTextExamples(folder,cl,n) :
-    """ Reads maximum n text files from folder and returns them as a list of
-        list of text and its class label cl."""
-    from glob import glob
-    x = [] # list of text examples and class labels
+def answer_question(context, question):
+    # Preprocess the context and question
+    inputs = tokenizer.encode_plus(question, context, add_special_tokens=True, return_tensors='tf')
 
-    files = glob(folder+"/*.txt") # read all text files files
-    for file in files :
-        infile = open(file,"r",encoding="utf-8")
-        data = infile.read()
-        infile.close()
-        x.append([data,cl])
-        if len(x)==n :
-            break
-    return x
+    # Retrieve the input IDs and attention masks
+    input_ids = inputs['input_ids']
+    attention_mask = inputs['attention_mask']
 
-def readPosNeg(pos_folder,neg_folder,n) :
-    """ Reads maximum n positive and maximum n negative text examples
-        from the respective folders. Randomizes them. Returns the list of
-        text and an np.array of corresponding one-hot class labels [1,0] for pos
-        and [0,1] for neg. """
-    pos = readTextExamples(pos_folder,[1,0],n)
-    neg = readTextExamples(neg_folder,[0,1],n)
-    allEg = pos + neg
-    random.shuffle(allEg)
-    x = []
-    y = []
-    for eg in allEg :
-        x.append(eg[0])
-        y.append(eg[1])
-    return x, np.array(y)
-### ### ### END Functions to read data
+    # Perform the question answering inference
+    start_scores, end_scores = model(input_ids, attention_mask=attention_mask)
 
-### ### ### train and test 
-def train_test() :
-    """ Training and testing for text classification using BERT. """
-    maxlen=512 # 512 maximum number of tokens
-    maxTrEg=1000 # maximum number of pos & neg training examples
-    maxTeEg=1000 # maximum number of pos & neg test examples
-    epochs=3 # number of epochs
+    # Get the most probable start and end positions
+    start_index = tf.argmax(start_scores, axis=1).numpy()[0]
+    end_index = tf.argmax(end_scores, axis=1).numpy()[0]
 
-    # read the data
-    train_x, train_y = readPosNeg("db/train/pos","aclImdb/train/neg",maxTrEg)
-    test_x, test_y = readPosNeg("db/test/pos","aclImdb/test/neg",maxTeEg)
+    # Convert the token IDs to actual tokens
+    tokens = tokenizer.convert_ids_to_tokens(input_ids.numpy()[0])
+    answer = tokenizer.convert_tokens_to_string(tokens[start_index:end_index+1])
 
-    # tokenize train and test set
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-    tokenized_train = tokenizer(train_x, max_length=maxlen, truncation=True,
-                                padding=True, return_tensors="tf")
-    tokenized_test = tokenizer(test_x, max_length=maxlen, truncation=True,
-                                padding=True, return_tensors="tf")
+    return answer
 
-    # build the model
-    bert_model = TFAutoModel.from_pretrained("bert-base-cased")
-    bert_model.trainable = False
-    
-    token_ids = Input(shape=(maxlen,), dtype=tf.int32,
-                                      name="token_ids")
-    attention_masks = Input(shape=(maxlen,), dtype=tf.int32,
-                                            name="attention_masks")
-    bert_output = bert_model(token_ids,attention_mask=attention_masks)
+# Load the SQuAD dataset
+processor = SquadV2Processor()
+train_examples = processor.get_train_examples('path/to/train.json')
+dev_examples = processor.get_dev_examples('path/to/dev.json')
 
-    output = Dense(2,activation="softmax")(bert_output[0][:,0])
+# Convert examples to features
+train_features = squad_convert_examples_to_features(
+    examples=train_examples,
+    tokenizer=tokenizer,
+    max_seq_length=384,
+    doc_stride=128,
+    max_query_length=64,
+    is_training=True,
+    return_dataset="tf",
+    threads=1,
+)
 
-    model = Model(inputs=[token_ids,attention_masks],outputs=output)
+dev_features = squad_convert_examples_to_features(
+    examples=dev_examples,
+    tokenizer=tokenizer,
+    max_seq_length=384,
+    doc_stride=128,
+    max_query_length=64,
+    is_training=False,
+    return_dataset="tf",
+    threads=1,
+)
 
-    # compile
-    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+# Create a TensorFlow dataset from the features
+train_dataset = train_features["dataset"]
+dev_dataset = dev_features["dataset"]
 
-    # train
-    model.fit([tokenized_train["input_ids"],tokenized_train["attention_mask"]],
-              train_y, batch_size=25, epochs=epochs)
+# Fine-tune the BERT model on the SQuAD dataset
+model = TFBertForQuestionAnswering.from_pretrained('bert-base-uncased')
+optimizer = tf.keras.optimizers.Adam(learning_rate=3e-5)
+model.compile(optimizer=optimizer, loss=model.compute_loss)
 
-    # evaluate
-    score = model.evaluate([tokenized_test["input_ids"],tokenized_test["attention_mask"]],test_y,verbose=0)
-    
-    print("Accuracy on test data:",score[1])
+model.fit(train_dataset.shuffle(100).batch(16), epochs=2, batch_size=16)
 
-    return model, [tokenized_train["input_ids"],tokenized_train["attention_mask"]], test_y
-### ### ###
+# Evaluate the fine-tuned model
+results = model.evaluate(dev_dataset.batch(16))
+print("Evaluation results:", results)
 
-
-# Run the code
-train_test()
-#End
+# Example usage
+context = "The quick brown fox jumps over the lazy dog."
+question = "What does the fox jump over?"
+answer = answer_question(context, question)
+print(answer)
