fb811a3412db7df712fe8344767cb62d37da576b
Bryan Miletta <bamiletta@gmail.com>
2023-06-25 14:32:51 -0500

    major update - changed to pytorch

--- taxAI.py
+++ taxAI.py
@@ -3,79 +3,117 @@
 #level: Sandbox
 #summary:this code is testing setting up a Q/A BERT model that allows user input. 
 
+
+
 ### ### ### Import Libraries
-import tensorflow as tf
-from transformers import BertTokenizer, TFBertForQuestionAnswering, TFBertForQuestionAnswering, TFBertModel
+import torch
+from transformers import BertTokenizer, BertForQuestionAnswering
 from transformers import squad_convert_examples_to_features
 from transformers.data.processors.squad import SquadV2Processor
-from transformers.data.metrics.squad_metrics import squad_evaluate
+from transformers.data.metrics.squad_metrics import compute_predictions_logits, squad_evaluate
 
 # Load the BERT model and tokenizer
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model_name = 'bert-base-uncased'
+model = BertForQuestionAnswering.from_pretrained(model_name)
+tokenizer = BertTokenizer.from_pretrained(model_name)
 
-def answer_question(context, question):
-    # Preprocess the context and question
-    inputs = tokenizer.encode_plus(question, context, add_special_tokens=True, return_tensors='tf')
+# Load the SQuAD 2.0 dataset
+train_file = "db"
+dev_file = "db"
 
-    # Retrieve the input IDs and attention masks
-    input_ids = inputs['input_ids']
-    attention_mask = inputs['attention_mask']
-
-    # Perform the question answering inference
-    start_scores, end_scores = model(input_ids, attention_mask=attention_mask)
-
-    # Get the most probable start and end positions
-    start_index = tf.argmax(start_scores, axis=1).numpy()[0]
-    end_index = tf.argmax(end_scores, axis=1).numpy()[0]
-
-    # Convert the token IDs to actual tokens
-    tokens = tokenizer.convert_ids_to_tokens(input_ids.numpy()[0])
-    answer = tokenizer.convert_tokens_to_string(tokens[start_index:end_index+1])
-
-    return answer
-
-# Load the SQuAD dataset
 processor = SquadV2Processor()
-train_examples = processor.get_train_examples("db")
-dev_examples = processor.get_dev_examples("db")
+train_examples = processor.get_train_examples(train_file)
+dev_examples = processor.get_dev_examples(dev_file)
 
 # Convert examples to features
-train_features = squad_convert_examples_to_features(
+train_features, train_dataset = squad_convert_examples_to_features(
     examples=train_examples,
     tokenizer=tokenizer,
     max_seq_length=384,
     doc_stride=128,
     max_query_length=64,
     is_training=True,
-    return_dataset="tf",
+    return_dataset="pt",
     threads=1,
 )
 
-dev_features = squad_convert_examples_to_features(
+dev_features, dev_dataset = squad_convert_examples_to_features(
     examples=dev_examples,
     tokenizer=tokenizer,
     max_seq_length=384,
     doc_stride=128,
     max_query_length=64,
     is_training=False,
-    return_dataset="tf",
+    return_dataset="pt",
     threads=1,
 )
 
-# Create a TensorFlow dataset from the features
-train_dataset = train_features["dataset"]
-dev_dataset = dev_features["dataset"]
+# Fine-tune the BERT model on the SQuAD 2.0 dataset
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-5)
 
-# Fine-tune the BERT model on the SQuAD dataset
-model = TFBertForQuestionAnswering.from_pretrained('bert-base-uncased')
-optimizer = tf.keras.optimizers.Adam(learning_rate=3e-5)
-model.compile(optimizer=optimizer, loss=model.compute_loss)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True)
+dev_loader = torch.utils.data.DataLoader(dev_dataset, batch_size=16)
 
-model.fit(train_dataset.shuffle(100).batch(16), epochs=2, batch_size=16)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+epochs = 2
+for epoch in range(epochs):
+    model.train()
+    total_loss = 0
+
+    for batch in train_loader:
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        start_positions = batch['start_positions'].to(device)
+        end_positions = batch['end_positions'].to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            start_positions=start_positions,
+            end_positions=end_positions
+        )
+
+        loss = outputs.loss
+        total_loss += loss.item()
+
+        loss.backward()
+        optimizer.step()
+
+    avg_train_loss = total_loss / len(train_loader)
+    print(f"Epoch {epoch+1}/{epochs} - Average training loss: {avg_train_loss}")
 
 # Evaluate the fine-tuned model
-results = model.evaluate(dev_dataset.batch(16))
-print("Evaluation results:", results)
+model.eval()
+all_predictions = []
+all_true_labels = []
+
+for batch in dev_loader:
+    input_ids = batch['input_ids'].to(device)
+    attention_mask = batch['attention_mask'].to(device)
+
+    with torch.no_grad():
+        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+
+    predictions = compute_predictions_logits(
+        batch,
+        outputs.start_logits,
+        outputs.end_logits,
+        tokenizer,
+        20,
+        30,
+        True
+    )
+
+    all_predictions.extend(predictions)
+    all_true_labels.extend(batch['answers'])
+
+# Evaluate the predictions
+dev_metrics = squad_evaluate(examples=dev_examples, preds=all_predictions, no_answer_probs=None)
+print("Evaluation metrics:", dev_metrics)
 
 # Example usage
 context = "The quick brown fox jumps over the lazy dog."
