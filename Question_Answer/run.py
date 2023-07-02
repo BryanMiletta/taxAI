6474d583cc9395333ab900640c8dc13b67daa137
Bryan Miletta <bamiletta@gmail.com>
2023-07-02 13:11:46 -0500

    Added UI for question answer

--- Question_Answer/run.py
+++ Question_Answer/run.py
@@ -4,10 +4,12 @@ from loadModel import *
 import textwrap
 import nltk
 
+question = input("\nPlease enter your question: \n")
+
 ### ### ### creates a dataset that pulls text from website
-#p = create_dataset.Create_DS()
-#url = 'https://en.wikipedia.org/wiki/Taxation_in_the_United_States'
-#p.loadArticle(url)
+p = create_dataset.Create_DS()
+url = 'https://en.wikipedia.org/wiki/Taxation_in_the_United_States'
+p.loadArticle(url)
 ### ### ###
 
 ### ### ### creates a dataset that pulls text from PDF
@@ -21,17 +23,33 @@ import nltk
 #p.loadTxt('State rules for determining taxable income often differ from federal rules. Federal marginal tax rates vary from 10% to 37% of taxable income.[3] State and local tax rates vary widely by jurisdiction, from 0% to 13.30% of income,[4] and many are graduated. State taxes are generally treated as a deductible expense for federal tax computation, although the 2017 tax law imposed a $10,000 limit on the state and local tax ("SALT") deduction, which raised the effective tax rate on medium and high earners in high tax states. Prior to the SALT deduction limit, the average deduction exceeded $10,000 in most of the Midwest, and exceeded $11,000 in most of the Northeastern United States, as well as California and Oregon.[5] The states impacted the most by the limit were the tri-state area (NY, NJ, and CT) and California; the average SALT deduction in those states was greater than $17,000 in 2014.[5]')
 ### ### ###
 
-model = QAPipe(p.ds)
-question = "What is the range of state and local tax rates?"
+while True:
+    model = QAPipe(p.ds)
+    answer_start_index,answer_end_index,start_token_score,end_token_score,s_Scores,e_Scores,answer=model.get_output(question)
+    wrapper = textwrap.TextWrapper(width=80)
+    print(wrapper.fill(p.ds)+"\n")
 
-answer_start_index,answer_end_index,start_token_score,end_token_score,s_Scores,e_Scores,answer=model.get_output(question)
-wrapper = textwrap.TextWrapper(width=80)
-print(wrapper.fill(p.ds)+"\n")
+    print("Question: "+question)
+    print("Answer : " + answer)
 
-print("Question: "+question)
-print("Answer : " + answer)
+    flag = True
+    flag_N = False
+    
+    while flag:
+        response = input("\nDo you want to ask another question based on this text (Y/N)? ")
+        if response[0] == "Y":
+            question = input("\nPlease enter your question: \n")
+            flag = False
+        elif response[0] == "N":
+            print("\nBye!")
+            flag = False
+            flag_N = True
+            
+    if flag_N == True:
+        break
 
 ### ### ### Model details
+'''
 tokens = model.generate_text_from_token()
 print("Passage: ")
 print(wrapper.fill(p.ds)+"\n")
@@ -110,3 +128,4 @@ g.set_xticklabels(g.ax.get_xticklabels(), rotation=90, ha="center")
 # Turn on the vertical grid to help align words to scores.
 g.ax.grid(True)
 
+'''
