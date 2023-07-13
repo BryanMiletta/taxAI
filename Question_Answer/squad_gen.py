98bfe57facd804fa848b1929eb8b059b61e4106d
Bryan Miletta <bmiletta@uwm.edu>
2023-07-13 10:56:49 -0500

    fixed bug to find correct answer start and end token ids

--- Question_Answer/squad_gen.py
+++ Question_Answer/squad_gen.py
@@ -28,56 +28,38 @@ model_name='bert-large-cased-whole-word-masking-finetuned-squad'
 from transformers import BertTokenizer
 # Load the tokenizer
 tokenizer = BertTokenizer.from_pretrained(model_name)
-
-### take the input file and itterate through the rows
-answers="what if i change this? because it is."
-
-# tokenize the text and calculate token IDs for answer_start and answer_end
 maxlen=512 # 512 maximum number of tokens
-# Tokenize the context, question, and answer
-#tokenized_context = tokenizer.encode_plus(context, add_special_tokens=True, truncation=True, max_length=maxlen, padding='max_length')
-#input_ids_context = tokenized_context['input_ids']
-#tokenized_question = tokenizer.encode_plus(question, add_special_tokens=True, truncation=True, max_length=maxlen, padding='max_length')
-#input_ids_question = tokenized_question['input_ids']
-tokenized_answers = tokenizer.encode_plus(answers, add_special_tokens=True, truncation=True, max_length=maxlen, padding=False)
-input_ids_answers = tokenized_answers['input_ids']
-# Find the token IDs for answer_start and answer_end positions
-answer_start_token = input_ids_answers[1] #finds the first token of the answer after the CLS token
-answer_end_token = input_ids_answers[-2] #finds the last token of the answer before the CLS token
+### take the input file and itterate through the rows
 
-print(answer_start_token)
-print(answer_end_token)
+# Open the input CSV file
+with open(input_file, 'r') as input_file:
+    # Open the output CSV file
+    with open(output_file, 'w', newline='') as output_file:
+        reader = csv.reader(input_file)
+        writer = csv.writer(output_file)
 
+         # Read and ignore the first row (headers)
+        headers = next(reader)
+        # Write the headers to the output file
+        writer.writerow(headers)
 
-
-
-'''
-# Process each line (excluding the header)
-for row in reader[0:]:
-    # Split the line into columns
-    columns = reader.strip().split(',')
-
-    # Extract the values from each row
-    context = columns[0]
-    question = columns[1]
-    answers = columns[3]
-    answer_start = columns[4]
-    answer_end = columns[5]
-
-    # Tokenize the data and calculate token IDs
-    tokenized_data = tokenize_data(context, question, answers, answer_start, answer_end)
-
-    # Access the token IDs for answer_start and answer_end
-    answer_start_token_id = tokenized_data['input_ids'][tokenized_data['answer_start_token']]
-    answer_end_token_id = tokenized_data['input_ids'][tokenized_data['answer_end_token']]
-
-    # Update the answer_start and answer_end columns with token IDs
-    columns[4] = str(answer_start_token_id)
-    columns[5] = str(answer_end_token_id)
-
-    # Join the updated columns
-    updated_line = ','.join(columns)
-
-    # Print or save the updated line as needed
-    print(updated_line)
-'''
+        # Iterate over each row in the input file
+        for row in reader:
+            # Read the value from column 3
+            answers = row[3]
+            # tokenize the text and calculate token IDs for answer_start and answer_end
+            # Tokenize the context, question, and answer
+            #tokenized_context = tokenizer.encode_plus(context, add_special_tokens=True, truncation=True, max_length=maxlen, padding='max_length')
+            #input_ids_context = tokenized_context['input_ids']
+            #tokenized_question = tokenizer.encode_plus(question, add_special_tokens=True, truncation=True, max_length=maxlen, padding='max_length')
+            #input_ids_question = tokenized_question['input_ids']
+            tokenized_answers = tokenizer.encode_plus(answers, add_special_tokens=True, truncation=True, max_length=maxlen, padding=False)
+            input_ids_answers = tokenized_answers['input_ids']
+            # Find the token IDs for answer_start and answer_end positions
+            answer_start_token = input_ids_answers[1] #finds the first token of the answer after the CLS token
+            answer_end_token = input_ids_answers[-3] #finds the last token of the answer before the CLS token, and goes back one to avoid punctuation
+            # Write the value to column 4
+            row[4] = answer_start_token
+            row[5] = answer_end_token
+            # Write the updated row to the output file
+            writer.writerow(row)
