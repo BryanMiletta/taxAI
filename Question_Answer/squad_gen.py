0ef1c131aa4b3f5600ffab9d06ddc8c226cda0fd
Bryan Miletta <bamiletta@gmail.com>
2023-07-13 10:39:10 -0500

    updated json generating script

--- Question_Answer/squad_gen.py
+++ Question_Answer/squad_gen.py
@@ -2,6 +2,10 @@
 #TaxAI
 #level: Proto
 #summary: Generates the SQuAD json traing file from a human generated csv (which came from a google sheet)
+### ### ### creates a dataset using text from:
+# https://www.irs.gov/faqs 
+# https://en.wikipedia.org/wiki/Taxation_in_the_United_States 
+### csv file format
 # column 1: context
 # column 2: question
 # column 3: question_id
@@ -15,45 +19,31 @@ import transformers
 from transformers import BertTokenizer, BertForQuestionAnswering
 import torch
 
-model_name='bert-large-uncased-whole-word-masking-finetuned-squad'
-
+### input/output files
 input_file = 'db/fine-tuning dataset - Sheet1.csv'
 output_file = 'db/updated_csv.csv'
 
-### ### ### creates a dataset using text from:
-# https://www.irs.gov/faqs 
-# https://en.wikipedia.org/wiki/Taxation_in_the_United_States 
-
+### BERT model for fine-tuning
+model_name='bert-large-uncased-whole-word-masking-finetuned-squad'
 from transformers import BertTokenizer
-
 # Load the tokenizer
 tokenizer = BertTokenizer.from_pretrained(model_name)
 
-context = "testing this."
-question="why is this so hard?"
+### take the input file and itterate through the rows
 answers="what if i change this? because it is."
 
 # tokenize the text and calculate token IDs for answer_start and answer_end
 maxlen=512 # 512 maximum number of tokens
-maxTrEg=1000 # maximum number of pos & neg training examples
-maxTeEg=1000 # maximum number of pos & neg test examples
-epochs=3 # number of epochs
 # Tokenize the context, question, and answer
-tokenized_context = tokenizer.encode_plus(context, add_special_tokens=True, truncation=True, max_length=maxlen, padding='max_length')
-input_ids_context = tokenized_context['input_ids']
-tokenized_question = tokenizer.encode_plus(question, add_special_tokens=True, truncation=True, max_length=maxlen, padding='max_length')
-input_ids_question = tokenized_question['input_ids']
+#tokenized_context = tokenizer.encode_plus(context, add_special_tokens=True, truncation=True, max_length=maxlen, padding='max_length')
+#input_ids_context = tokenized_context['input_ids']
+#tokenized_question = tokenizer.encode_plus(question, add_special_tokens=True, truncation=True, max_length=maxlen, padding='max_length')
+#input_ids_question = tokenized_question['input_ids']
 tokenized_answers = tokenizer.encode_plus(answers, add_special_tokens=True, truncation=True, max_length=maxlen, padding=False)
 input_ids_answers = tokenized_answers['input_ids']
-
 # Find the token IDs for answer_start and answer_end positions
 answer_start_token = input_ids_answers[1] #finds the first token of the answer after the CLS token
 answer_end_token = input_ids_answers[-2] #finds the last token of the answer before the CLS token
-
-# Update the tokenized_input dictionary with token IDs
-#tokenized_input['answer_start_token'] = answer_start_token
-#tokenized_input['answer_end_token'] = answer_end_token
-
 
 print(answer_start_token)
 print(answer_end_token)
