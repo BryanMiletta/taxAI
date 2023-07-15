e4fd441db00a5433b4fd60b5e982b4546e35abbf
Bryan Miletta <bamiletta@gmail.com>
2023-07-15 10:16:36 -0500

    new python script to turn csv into json format for SQuAD

--- Question_Answer/csv_to_json.py
+++ Question_Answer/csv_to_json.py
@@ -1,10 +1,51 @@
 import pandas as pd
+import json
 
+def convert_csv_to_squad(csv_file, json_file):
+    # Read CSV file
+    df = pd.read_csv(csv_file)
+
+    # Initialize SQuAD JSON structure
+    squad_data = {
+        'data': []
+    }
+
+    paragraphs = []
+
+    # Iterate over each row in the CSV
+    for index, row in df.iterrows():
+        context = row['context']
+        question = row['question']
+        answers = row['answers']
+        answer_start = row['answer_start']
+
+        # Create a SQuAD paragraph entry
+        paragraph = {
+            'context': context,
+            'qas': [{
+                'question': question,
+                'id': str(index),
+                'answers': [{
+                    'text': answers,
+                    'answer_start': answer_start
+                }]
+            }]
+        }
+
+        paragraphs.append(paragraph)
+
+    # Create a SQuAD data entry
+    squad_data['data'].append({
+        'title': 'TaxAI Dataset',
+        'paragraphs': paragraphs
+    })
+
+    # Save the SQuAD JSON file
+    with open(json_file, 'w') as outfile:
+        json.dump(squad_data, outfile)
+
+# Convert CSV to SQuAD JSON
 csv_file = 'db/updated_csv.csv'
 json_file = 'db/json_file.json'
+convert_csv_to_squad(csv_file, json_file)
 
-# Read CSV file
-df = pd.read_csv(csv_file)
-
-# Convert DataFrame to JSON
-df.to_json(json_file, orient='records', lines=True)
