751c58f9eaac5860ff8f97e9b3b41c4201415dfc
Bryan Miletta <bmiletta@uwm.edu>
2023-07-19 14:37:04 -0500

    fixed context extraction

--- Sandbox/newspaper_test.py
+++ Sandbox/newspaper_test.py
@@ -5,15 +5,13 @@ import nltk
 import PyPDF2
 from PyPDF2 import PdfReader
 
-
-
 p = newspaper_sandbox.Create_DS()
 #p.loadTxt("John is a 10 year old boy. He is the son of Robert Smith.  Elizabeth Davis is Robert's wife. She teaches at UC Berkeley. Sophia Smith is Elizabeth's daughter. She studies at UC Davis")
 #print(p.ds)
 
 url = 'https://en.wikipedia.org/wiki/Taxation_in_the_United_States'
-text = p.loadArticle(url)
-'''
+p.loadArticle(url)
+
 # Step 1: Extract text from the PDF file
 def extract_text_from_pdf(file_path):
     with open(file_path, 'rb') as file:
@@ -29,15 +27,15 @@ def process_text(text):
     article.set_text(text)
     article.parse()
     return article.title, article.text
-'''
+
 # Step 3: Call the functions to extract and process the PDF text
-pdf_file_path = 'db/f1040_filled.pdf'
-extracted_text = extract_text_from_pdf(pdf_file_path)
+#pdf_file_path = 'db/f1040_filled.pdf'
+#extracted_text = extract_text_from_pdf(pdf_file_path)
 #title, text = process_text(extracted_text)
 
 # Step 4: Print the extracted title and text
 #print("Title:", title)
 #print("Text:", text)
-print(text)
+print(p.ds)
         
 
